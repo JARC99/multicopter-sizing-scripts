1cfb63f2dc2493f4766cb5de4288dd050f7b0385
José A. Rosas <jarosascordova@gmail.com>
2023-01-19 13:10:29 -0600

    define script outline

---
+++ opt_motor_prop/opt_motor_prop.py
@@ -0,0 +1,23 @@
+"""Multicopter performance evaluation script"""
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from fluids import ATMOSPHERE_1976
+from tabulate import tabulate
+
+from scipy.optimize import fsolve
+
+sns.set_theme(style="whitegrid")
+GRAPHICS_PATH = "output/"
+DPI = 300
+
+# %% Motor characteristics
+
+# %% Blade number studies
+
+# %% Pitch studies
+
+# %% Diameter studies
